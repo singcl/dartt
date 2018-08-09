9228b91e987a9d01863ae35bd7b29dbf775be75c
singcl <24661881@qq.com>
2018-08-09 22:35:28 +0800

    :art: add main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+// Define a function.
+void printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+void main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
+// dart --checked hello.dart
+
+var c = 'sss''ssss''sssssddd';
+int a = 4;
+double f = 3.444;
+num sssss = 3;
+const  i = 10;
+String name = 'sssss';
+Function hee;
+
+Function sayHello2 = (name) => 'ssss';
+String sayHello(name) => 'Hello $name!';

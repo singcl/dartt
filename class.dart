74081977e3ae2cb441807da3f755bcff09b633e5
singcl <24661881@qq.com>
2018-08-14 01:25:36 +0800

    :art: simple class

--- class.dart
+++ class.dart
@@ -1,1 +1,11 @@
 // dart class learning...
+class Ponit {
+  num x;
+  num y;
+  Ponit(this.x, this.y);
+}
+
+void main(List<String> args) {
+  var point = Ponit(1, 2);
+  print("x is ${point.x}, y is ${point.y}");
+}

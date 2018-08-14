0bd8b7fdf65cf2d176f46462b2544f92758e45f0
singcl <24661881@qq.com>
2018-08-14 11:20:04 +0800

    dart中函数的四种常见写法

--- main.dart
+++ main.dart
@@ -1,22 +1,33 @@
-// Define a function.
-void printInteger(int aNumber) {
-  print('The number is $aNumber.'); // Print to console.
-}
-
 // This is where the app starts executing.
 void main() {
   var number = 42; // Declare and initialize a variable.
   printInteger(number); // Call a function.
+  print(sayHello("Singcl"));
+  print(sayHello2("Singcl"));
 }
 // dart --checked hello.dart
 
-var c = 'sss''ssss''sssssddd';
+var c = 'sss' 'ssss' 'sssssddd';
 int a = 4;
 double f = 3.444;
 num sssss = 3;
-const  i = 10;
+const i = 10;
 String name = 'sssss';
 Function hee;
 
-Function sayHello2 = (name) => 'ssss';
-String sayHello(name) => 'Hello $name!';
+// 函数的多种写法
+
+// 声明一个Function类型的变量 - 非箭头函数写法
+Function sayHello3 = (String name) {
+  var x = 4;
+  return x;
+};
+// 声明一个Function类型的变量 - 类似js的[箭头函数]函数表达式
+Function sayHello2 = (String name) => 'Hello2 $name';
+
+// 定义一个箭头函数 - js中没有这种写法。 Js箭头函数只有上面那种写法
+String sayHello(String name) => 'Hello $name!';
+// Define a function.
+void printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
